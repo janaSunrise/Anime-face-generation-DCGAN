7ffc2b21392c2541e360382063ae7f258550d6dd
janaSunrise <warriordefenderz@gmail.com>
2021-04-26 17:29:44 +0530

    Added the pytorch script for generating images and video.

---
+++ src/anime_gan_exported_model.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+This file is for demostration on How to use exported models, by loading them and generating
+images using them.
+"""
+
+import os
+
+import cv2
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from torchvision.utils import make_grid
+from torchvision.utils import save_image
+
+# Define the constants needed
+latent_size = 128
+batch_size = 128
+stats = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+
+# Define the utility methods we need
+def denorm(img_tensors):
+    return img_tensors * stats[1][0] + stats[0][0]
+
+def show_images(images, nmax=64):
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.set_xticks([]); ax.set_yticks([])
+    ax.imshow(make_grid(denorm(images.detach()[:nmax]), nrow=8).permute(1, 2, 0))
+
+def show_batch(dl, nmax=64):
+    for images, _ in dl:
+        show_images(images, nmax)
+        break
+
+# Define the models and load the exported models
+discriminator = nn.Sequential(
+    # in: 3 x 64 x 64
+
+    nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(64),
+    nn.LeakyReLU(0.2, inplace=True),
+    # out: 64 x 32 x 32
+
+    nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(128),
+    nn.LeakyReLU(0.2, inplace=True),
+    # out: 128 x 16 x 16
+
+    nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(256),
+    nn.LeakyReLU(0.2, inplace=True),
+    # out: 256 x 8 x 8
+
+    nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(512),
+    nn.LeakyReLU(0.2, inplace=True),
+    # out: 512 x 4 x 4
+
+    nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
+    # out: 1 x 1 x 1
+
+    nn.Flatten(),
+    nn.Sigmoid()
+)
+
+generator = nn.Sequential(
+    # in: latent_size x 1 x 1
+
+    nn.ConvTranspose2d(latent_size, 512, kernel_size=4, stride=1, padding=0, bias=False),
+    nn.BatchNorm2d(512),
+    nn.ReLU(True),
+    # out: 512 x 4 x 4
+
+    nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(256),
+    nn.ReLU(True),
+    # out: 256 x 8 x 8
+
+    nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(128),
+    nn.ReLU(True),
+    # out: 128 x 16 x 16
+
+    nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(64),
+    nn.ReLU(True),
+    # out: 64 x 32 x 32
+
+    nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.Tanh()
+    # out: 3 x 64 x 64
+)
+
+# Load the Saved models
+## MAP Them to use CPU, so It would work for any machine
+generator.load_state_dict(torch.load("generator_model.bin", map_location=torch.device('cpu')))
+discriminator.load_state_dict(torch.load("discriminator_model.bin", map_location=torch.device('cpu')))
+
+# Finally, Evaluate them
+generator.eval()
+discriminator.eval()
+
+# Define the method we need for generating, and saving images
+sample_dir = 'generated'
+os.makedirs(sample_dir, exist_ok=True)
+
+def save_samples(index, show=True):
+    latent_tensors = torch.randn(64, latent_size, 1, 1, device=torch.device('cpu'))
+
+    fake_images = generator(latent_tensors)
+    fake_fname = 'generated-images-{0:0=4d}.png'.format(index)
+
+    save_image(denorm(fake_images), os.path.join(sample_dir, fake_fname), nrow=8)
+    print('Saving', fake_fname)
+
+    if show:
+        fig, ax = plt.subplots(figsize=(8, 8))
+        ax.set_xticks([]); ax.set_yticks([])
+        ax.imshow(make_grid(fake_images.cpu().detach(), nrow=8).permute(1, 2, 0))
+
+# Since it works pretty well, We can now generate images
+for epoch in range(20):
+  save_samples(epoch + 1, show=False)
+
+# That worked great! Time to export into a video!
+def generate_video(generated_location, save_filename):
+    files = [os.path.join(sample_dir, f) for f in os.listdir(sample_dir) if generated_location in f]
+    files.sort()
+
+    out = cv2.VideoWriter(save_filename, cv2.VideoWriter_fourcc(*'MP4V'), 1, (530,530))
+    [out.write(cv2.imread(fname)) for fname in files]
+    out.release()
+
+    print("Done, Exported as {}".format(save_filename))
+
+generate_video("generated", "exported_model_generation.avi")
